3bed542e2fd44a89f624d0ab9a622ef0c5ae8c7c
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-10 03:14:45 +0000

    header file

---
+++ C_01/include.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void ft_ft(int *nbr);
+
+void ft_div_mod(int a, int b, int *div, int *mod);
+
+void ft_putstr(char *str);
+
+void ft_rev_int_tab(int *tab, int size);
+
+int ft_strlen(char *str);
+
+void ft_swap(int *a, int *b);
+
+void ft_ultimate_div_mod(int *a, int *b);
+
+#endif INCLUDE_H
